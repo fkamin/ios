c936c39a625790c81699e009d4adc1a6bffa4b2a
fkamin <s95429@pollub.edu.pl>
2024-10-15 12:21:13 +0200

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by student on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
