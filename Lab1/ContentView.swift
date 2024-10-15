65dc0751f8e01df9ed83b9232fea6816f21b99f4
Filip Kamiński <92945318+fkamin@users.noreply.github.com>
2024-10-15 13:03:52 +0200

    Komit1

--- Lab1/ContentView.swift
+++ Lab1/ContentView.swift
@@ -1,21 +1,30 @@
-//
-//  ContentView.swift
-//  Lab1
-//
-//  Created by student on 15/10/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var arraysOfColor: [ColorData] = [
+        ColorData.init(colorName: "red", colorValue: Color.red),
+        ColorData.init(colorName: "blue", colorValue: Color.blue),
+        ColorData.init(colorName: "yellow", colorValue: Color.yellow),
+        ColorData.init(colorName: "green", colorValue: Color.green),
+        ColorData.init(colorName: "purple", colorValue: Color.purple),
+        ColorData.init(colorName: "orange", colorValue: Color.orange),
+        ColorData.init(colorName: "black", colorValue: Color.black),
+        ColorData.init(colorName: "gray", colorValue: Color.gray),
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle().fill(Color.red).opacity(0.5)
+            Circle().fill(Color.blue).opacity(0.5)
+            Circle().fill(randColor())
         }
         .padding()
+    }
+    
+    func randColor() -> Color {
+        let randomColor = Int.random(in: 0..<8)
+        //arraysOfColor[randomColor].incCount()
+        return arraysOfColor[randomColor].colorValue
     }
 }
 
