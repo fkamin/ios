22ab808c028e4bafd1dd71fc16b747602b4664d2
Filip Kamiński <92945318+fkamin@users.noreply.github.com>
2024-10-22 13:13:28 +0200

    komin

---
+++ lab2/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by student on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
